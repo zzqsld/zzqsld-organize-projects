44f19dbe3ce2e5611221f28a8dc99ae7a09cf1a6
zzqsld <zzqsld@gmail.com>
2025-12-14 13:42:48 +0800

    chore: stop tracking venv directory

--- venv/Lib/site-packages/pypinyin/exceptions.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Union, Text, ByteString
-
-
-class PinyinNotFoundException(Exception):
-    message: Union[Text, ByteString]
-    chars: Union[Text, ByteString]
-
-    def __init__(self, chars: Union[Text, ByteString]) -> None: ...
